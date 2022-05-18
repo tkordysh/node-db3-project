163cea738517cc715ebf4957e3148c9959fc1646
Tony Kordysh <tkordysh@gmail.com>
2022-05-18 17:01:22 -0500

    checkSchemeId middleware working

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const db = require("../../data/db-config");
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,10 +8,25 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-//t
-const checkSchemeId = (req, res, next) => {
 
-}
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const existing = await db("schemes")
+      .where("scheme_id", req.params.scheme_id)
+      .first();
+
+    if (!existing) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,9 +36,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,12 +47,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
