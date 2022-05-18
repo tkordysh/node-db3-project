a1cd98be2ed53977a22a8aeaca475debf435eab9
Tony Kordysh <tkordysh@gmail.com>
2022-05-18 17:08:29 -0500

    validateScheme middleware working

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -36,7 +36,18 @@ const checkSchemeId = async (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {};
+const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    scheme_name !== "string" ||
+    !scheme_name.trim()
+  ) {
+    next({ status: 400, message: "invalid scheme_name" });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
